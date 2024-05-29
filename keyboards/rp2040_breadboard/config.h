7884656fbef832415e769eb135832ba18f24c9eb
kikkidev64 <kikkidev64@gmail.com>
2024-05-30 00:37:24 +0900

    12CD1に変更

--- keyboards/rp2040_breadboard/config.h
+++ keyboards/rp2040_breadboard/config.h
@@ -1,4 +1,6 @@
-#define I2C_DRIVER I2CD0
+// #define I2C_DRIVER I2CD0
+#define I2C_DRIVER I2CD1
 
-#define I2C1_SDA_PIN GP4
-#define I2C1_SCL_PIN GP5
+
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
