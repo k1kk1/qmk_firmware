8b1f3152afc6cb34609286f3df907105c5a436df
kikkidev64 <kikkidev64@gmail.com>
2024-05-30 01:07:39 +0900

    OLED対応

--- keyboards/key1a/config.h
+++ keyboards/key1a/config.h
@@ -9,7 +9,7 @@
 
 #define HAL_USE_I2C TRUE
 
-#define I2C_DRIVER I2CD0
-#define I2C1_SDA_PIN GP5
-#define I2C1_SCL_PIN GP6
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
 
--- keyboards/key1a/mcuconf.h
+++ keyboards/key1a/mcuconf.h
@@ -1,9 +1,10 @@
 #pragma once
 #include_next "mcuconf.h"
 
+// デフォルトはRP_I2C_USE_I2C1
+// I2C0を使う場合はRP_I2C_USE_I2C0を定義する
+// #undef RP_I2C_USE_I2C0
+// #define RP_I2C_USE_I2C0 TRUE
 
-#undef RP_I2C_USE_I2C0
-#define RP_I2C_USE_I2C0 TRUE
-
-#undef RP_I2C_USE_I2C1
-#define RP_I2C_USE_I2C1 FALSE
+// #undef RP_I2C_USE_I2C1
+// #define RP_I2C_USE_I2C1 FALSE
